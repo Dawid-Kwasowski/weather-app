dc37f41aeba803e45f9718d1e3b2ae8895515985
Dawid Kwasowski <dawidkwasowski@gmail.com>
2020-07-17 00:17:27 +0200

    Create & Call geo and weather api
    Create a function which is calling a current geolocation
    longitudes and latitudes then insert this data into fetch endpoint.
    --------------------------------------------------------------------

--- js/main.js
+++ js/main.js
@@ -1,3 +1,33 @@
+addEventListener('DOMContentLoaded', () => {
+   // function which returns the current device position
+   const getPosition = (callback) => {
+      return navigator.geolocation.getCurrentPosition(succes => {
+         return callback(succes)
+      })
+   }
+
+   return new Promise((resolve,reject) => {
+      // shordhand if statement which checking geoloc support 
+      return navigator.geolocation ? getPosition(resolve) : console.error('The Geolocation has not supported at this browser')
+   })
+   .then(response => {
+      const lat = response.coords.latitude
+      const lon = response.coords.longitude
+
+      const apiCall = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=8b48a6bae4902b0c459a89225cd3615a`
+      fetch(apiCall)
+      .then(response => response.json())
+      .then(data => {
+         const cityName = document.querySelector('.localization__city')
+         const countryName = document.querySelector('.localization__country')
+         return (
+            cityName.textContent = data.name,
+            countryName.textContent = data.sys.country
+         )
+      })
+   })
+})
+
 const WeatherApp = () => {
    const getCurrentTime = () => {
       const timeHolder = document.querySelector('.date__time')
@@ -19,11 +49,13 @@ const WeatherApp = () => {
 
    const getForecast = () => {
       const form = document.querySelector('.form-group')
+      const citySearch = document.querySelector('.form-group__search')
+
       form.addEventListener('submit', e => {
          e.preventDefault()
-         alert('GOT IT')
       })
    }
+
    getForecast()
    getCurrentDate()
 }
